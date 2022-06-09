48b88c3bcf7de081aedd0e18d7fb31510affdbc7
tour9566 <1575165408@qq.com>
2022-06-09 17:15:16 +0800

    spark、flink

--- elasticsearch/src/main/java/com/example/demo/schedulTask/Test.java
+++ elasticsearch/src/main/java/com/example/demo/schedulTask/Test.java
@@ -1,14 +1,16 @@
 package com.example.demo.schedulTask;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class Test {
     @Scheduled(cron = "0/1 * * * * ?")
     public void test(){
-//        log.info("logback的日志信息过来了");
-//        log.error("logback的错误信息过来了");
+        log.info("logback的日志信息过来了");
+        log.error("logback的错误信息过来了");
         System.out.println("1111111");
     }
 }
---
+++ spark/src/main/scala/spark/SparkApplication.java
@@ -0,0 +1,13 @@
+package spark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparkApplication.class, args);
+    }
+
+}
